66c64836d45abf791cd73b0e76bea1fbecb63556
unknown <Amrita>
2019-09-10 16:57:20 +0530

    first python program

---
+++ PythonForLife.py
@@ -0,0 +1,20 @@
+s='python for life'
+print(s[0:11],s[11:15].upper())
+# to print Python for LIFE
+t='python for life'
+print(t[0:6].upper(),t[7:10],t[11:15].upper())
+# to print PYTHON for LIFE
+w='python for life'
+print(w[0:6])
+print(w[7:10])
+print(w[11:15])
+print(w[2:6],w[7:10])
+print(w[8:10],w[11:13])
+# tp print python
+# for
+# life
+# thon for
+# or li
+
+
+
